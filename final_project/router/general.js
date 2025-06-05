b546b46b0ec900bb2a7c5e58064a6374204a13a1
bilal <bilalraza9170@gmail.com>
2025-06-06 00:49:14 +0500

    Complete task five

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -60,14 +60,20 @@ public_users.get("/title/:title", function (req, res) {
   if (matchedBooks.length > 0) {
     res.send(matchedBooks);
   } else {
-    res.status(404).json({ message: "Author not found" });
+    res.status(404).json({ message: "Title not found" });
   }
 });
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book) {
+    res.send(book.reviews);
+  } else {
+    res.status(404).json({ message: "Review not found" });
+  }
 });
 
 module.exports.general = public_users;
