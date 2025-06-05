74faafeec61822d4605f311db6d2e427993be51f
bilal <bilalraza9170@gmail.com>
2025-06-06 00:40:54 +0500

    Complete third task

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,8 +28,21 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const authorParam = req.params.author;
+  const matchedBooks = [];
+
+  Object.keys(books).forEach((bookId) => {
+    const bookAuthor = books[bookId].author;
+    if (bookAuthor === authorParam) {
+      matchedBooks.push({ id: bookId, ...books[bookId] });
+    }
+  });
+
+  if (matchedBooks.length > 0) {
+    res.send(matchedBooks);
+  } else {
+    res.status(404).json({ message: "Author not found" });
+  }
 });
 
 // Get all books based on title
